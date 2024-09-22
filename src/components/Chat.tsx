18f02df6e0e6547e882bf8074aba77f8e6b6beb6
Yaniv Bialik <yanivv77@gmail.com>
2024-09-22 20:43:39 +0300

    Add ChatInput component for chat input functionality

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -2,24 +2,22 @@
 
 import { useChat } from "ai/react"
 import { Messages } from "./Messages";
+import { ChatInput } from "./ChatInput";
 
 
 const Chat = ({ sessionId }: { sessionId: string }) => {
 
-  const { messages, handleInputChange, handleSubmit, input } = useChat({
+  const { messages, handleInputChange, handleSubmit, input ,setInput} = useChat({
     api: "/api/chat-stream",
     body: { sessionId },
   });
 
-  return <div className="relative min-h-full bg-zinc-900 flex divide-y divide-zinc-800 flex-col justify-between gap-2">
+  return <div className="relative min-h-full bg-zinc-900 flex divide-y divide-zinc-700 flex-col justify-between gap-2">
     <div className="flex-1 text-black bg-zinc-800 justify-between flex flex-col" >
       <Messages messages={messages}/>
     </div>
 
-    <form onSubmit={handleSubmit} className="p-2">
-       <input type="text" className="text-black" value={input} onChange={handleInputChange} />
-       <button type="submit" >send</button>
-    </form>
+   <ChatInput input={input} handleInputChange={handleInputChange} handleSubmit={handleSubmit} SetInput={setInput} />
   </div>
 
 }
