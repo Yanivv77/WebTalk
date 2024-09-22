26ca27f74771f240ce31eb854ec2ead4ada1dc93
Yaniv Bialik <yanivv77@gmail.com>
2024-09-22 19:51:15 +0300

    Add Chat

--- src/lib/rag-chat.ts
+++ src/lib/rag-chat.ts
@@ -1,5 +1,5 @@
 import { RAGChat, upstash } from "@upstash/rag-chat";
 
 export const ragChat = new RAGChat({
-  model: upstash('mistralai/Mistral-7B-Instruct-v0.2'),
+  model: upstash('meta-llama/Meta-Llama-3-8B-Instruct'),
 });
