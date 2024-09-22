ff30f07a7358e1477ce0b6e401411c8782184709
Yaniv Bialik <yanivv77@gmail.com>
2024-09-22 19:52:01 +0300

    Add database and api

--- src/app/[...url]/page.tsx
+++ src/app/[...url]/page.tsx
@@ -1,12 +1,36 @@
+import Chat from "@/components/Chat";
+import { ragChat } from "@/lib/rag-chat";
+import { redis } from "@/lib/redis";
+
 interface PageProps {
   params: {
     url: string | string[] | undefined;
   };
 }
 
-const Page = ({ params }: PageProps) => {
-  console.log(params);
-  return <div>Page</div>;
+function reconstructUrl({url}:{url: string[]}) {
+  const decodedComponents = url.map((component) => decodeURIComponent(component))
+  return decodedComponents.join('/')
+}
+
+const Page = async ({ params }: PageProps) => {
+
+  const reconstructedUrl = reconstructUrl({ url: params.url as string[] })
+  const isAReadyIndexed = await redis.sismember("indexed",reconstructedUrl)
+
+  const sessionId = "moke-session"
+
+  if(!isAReadyIndexed){
+    await ragChat.context.add({
+      type:"html",
+      source:reconstructedUrl,
+      config:{chunkOverlap:50,chunkSize:200}
+    })
+
+    await redis.sadd("indexed",reconstructedUrl)
+  }
+
+  return <Chat sessionId={sessionId}/>
 };
 
 export default Page;
